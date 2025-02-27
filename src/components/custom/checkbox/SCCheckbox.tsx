12a67b5320a967f7070be1d091d05a954d9d9f2e
edentns-sjju <sjju@edentns.com>
2025-02-27 16:12:29 +0900

    ⚡️Refactor: 체크박스 Label 클릭시 toggle 처리가 되도록 htmlFor 옵션 추가

--- src/components/custom/checkbox/SCCheckbox.tsx
+++ src/components/custom/checkbox/SCCheckbox.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useId } from 'react';
+
 import { Checkbox } from '@/components/ui/checkbox';
 
 interface SCCheckboxProps extends React.ComponentPropsWithoutRef<typeof Checkbox> {
     label?: string;
 }
 
-export const SCCheckbox = React.memo(({ label, className, ...props }: SCCheckboxProps) => {
+export const SCCheckbox = React.memo(({ label, className, id: propId, ...props }: SCCheckboxProps) => {
+    const generatedId = useId();
+    const id = propId || generatedId;
+
     return (
         <div className="flex items-center space-x-2">
-            <Checkbox className={className} {...props} />
+            <Checkbox id={id} className={className} {...props} />
             {label && (
                 <label
                     className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    htmlFor={props.id}
+                    htmlFor={id}
                 >
                     {label}
                 </label>
