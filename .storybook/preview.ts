ad3a50b6607a9a21f8a48b6e36c7dc9f2a5fcb37
edentns-sjju <sjju@edentns.com>
2025-03-11 16:25:46 +0900

    🔨Fix: 스토리북 실행 시 스크립트 오류 수정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,11 +4,10 @@ import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
     parameters: {
-        actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
-                date: /Date$/i,
+                date: /Date$/,
             },
         },
         options: {
