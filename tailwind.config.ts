6832686504585840250889d230d08f4870ba04ea
edentns-sjju <sjju@edentns.com>
2025-03-11 15:42:42 +0900

    🔥Refactor:  tailwind 미사용 함수 삭제

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-/**
- * 매개변수로 전달 받은 end 값 만큼 px 값을 rem 단위로 매핑한 배열을 반환
- * @param end 범위에 포함할 최대 픽셀 값
- * @param baseFontSize 기본 폰트 사이즈, default 16
- * @returns {Record<number, string>}
- */
-const pxToRem = (end: number, baseFontSize: number = 16): Record<number, string> => {
-    return Object.fromEntries(
-        Array.from({ length: end + 1 }).map((_, i) => {
-            let remValue = (i / baseFontSize).toFixed(3);
-            remValue = parseFloat(remValue).toString();
-            return [Number(i), `${remValue}rem`];
-        }),
-    );
-};
-
 export default {
     darkMode: ['class'],
     // Tailwind를 적용할 경로를 설정합니다.
